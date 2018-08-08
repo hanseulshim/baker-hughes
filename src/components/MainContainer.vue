6772b6a60b63597d8ade079e1fe3958288e4c847
Hanseul Shim <hanseulshim@gmail.com>
2018-08-08 16:05:53 -0400

    move header into MainContainer

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Header />
+    <CostChart
+      title='Cost over Time'
+      x-label='Time (hrs)'
+      x-data='cumulativeTime'
+      chart-accessor='cumulative-time'
+    />
+    <CostChart
+      title='Cost over Depth'
+      x-label='Depth (ft)'
+      x-data='cumulativeDepth'
+      chart-accessor='cumulative-depth'
+    />
+  </div>
+</template>
+
+<script>
+import Header from './Header';
+import CostChart from './CostChart';
+
+export default {
+  name: 'MainContainer',
+  components: {
+    Header,
+    CostChart,
+  },
+};
+</script>
