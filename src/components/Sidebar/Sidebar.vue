dba6dc9639dc7ab67624780a46932c5b30af7cd2
Hanseul Shim <hanseulshim@gmail.com>
2018-08-08 16:05:33 -0400

    add sidebar component

---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id='sidebar'>
+    <div class='sidebar-title'>Phantom</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+};
+</script>
+
+<style lang="sass">
+#sidebar
+  background-color: #3E3E3E
+  width: 250px
+  padding: 25px
+  box-sizing: border-box
+.sidebar-title
+  color: #FFF
+  font-size: 2em
+</style>
+
