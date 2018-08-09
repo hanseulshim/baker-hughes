de83cb609eba842d8a8e5b5faf6ad8e0eb9ea900
Hanseul Shim <hanseulshim@gmail.com>
2018-08-09 11:10:43 -0400

    add material icons and menu items

---
+++ src/components/Sidebar/MenuContainer.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id='menu-container'>
+    <div
+      class='menu-item'
+      v-for='menu in menuItems'
+      :key='menu.name'
+    >
+      <i class='material-icons'>{{menu.icon}}</i>
+      <span class='menu-item-name'>{{menu.name}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'menuContainer',
+  data() {
+    return {
+      menuItems: [
+        {
+          name: 'Dashboard',
+          icon: 'view_quilt',
+          route: '',
+        },
+        {
+          name: 'My Fields',
+          icon: 'view_list',
+          route: '',
+        },
+        {
+          name: 'Actions',
+          icon: 'offline_bolt',
+          route: '',
+        },
+        {
+          name: 'Performance',
+          icon: 'trending_up',
+          route: '',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="sass">
+#menu-container
+  color: #C5C5C5
+.menu-item
+  display: flex
+  align-items: center
+  padding: 10px 0 10px 20px
+  font-size: 1.3em
+  &:hover
+    cursor: pointer
+.menu-item-name
+  margin-left: 10px
+</style>
--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,23 +1,30 @@
 <template>
   <div id='sidebar'>
     <div class='sidebar-title'>Phantom</div>
+    <MenuContainer />
   </div>
 </template>
 
 <script>
+import MenuContainer from './MenuContainer';
+
 export default {
   name: 'Sidebar',
+  components: {
+    MenuContainer,
+  },
 };
 </script>
 
 <style lang="sass">
 #sidebar
   background-color: #3E3E3E
-  width: 250px
-  padding: 25px
+  width: 20%
+  min-width: 250px
   box-sizing: border-box
 .sidebar-title
   color: #FFF
   font-size: 2em
+  margin: 1em 1em 1em 20px
 </style>
 
