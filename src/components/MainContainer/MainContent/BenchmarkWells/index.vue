175e2906c8e83febaa2d84c121bc1ba9c1dc0829
Hanseul Shim <hanseulshim@gmail.com>
2018-08-10 11:04:32 -0400

    add input cost

--- src/components/MainContainer/MainContent/BenchmarkWells/index.vue
+++ src/components/MainContainer/MainContent/BenchmarkWells/index.vue
@@ -1,23 +1,17 @@
 <template>
   <div>
-    <v-card class="card-container">
-      <v-card-title class="card-container-title-row">
-        <div class="title-arrow-container">
-          <span class="title">Input Cost</span>
-          <i class="material-icons">keyboard_arrow_down</i>
-        </div>
-        <v-btn class="button-primary">update</v-btn>
-      </v-card-title>
-      <v-card-text>
-        test
-      </v-card-text>
-    </v-card>
+    <input-cost />
   </div>
 </template>
 
 <script>
+import InputCost from './InputCost';
+
 export default {
   name: 'benchmark-wells',
+  components: {
+    InputCost,
+  },
 };
 </script>
 
