02db6d0fd1b73e941e6b694faddc1d52434d208d
Hanseul Shim <hanseulshim@gmail.com>
2018-08-10 09:54:34 -0400

    move apps, style button, style card

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@ export default {
 
 <style lang="sass">
 @import "./assets/styles/cards.sass"
+@import "./assets/styles/button.sass"
 
 html, body, #app
   height: 100%
---
+++ src/components/MainContainer/MainContent/BenchmarkWells.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-card class="card-container">
+      <v-card-title class="card-container-title-row">
+        <div class="title-arrow-container">
+          <span class="title">Input Cost</span>
+          <i class="material-icons">keyboard_arrow_down</i>
+        </div>
+        <v-btn class="button-primary">update</v-btn>
+      </v-card-title>
+      <v-card-text>
+        test
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'benchmark-wells',
+};
+</script>
+
