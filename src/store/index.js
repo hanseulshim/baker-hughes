662f2c5c98050762c38a3879ea18443615c69907
Hanseul Shim <hanseulshim@gmail.com>
2018-08-23 14:26:21 -0400

    remove data we don't need anymore - move yMax into store

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import data from '../data/data.json';
 import dataPhantom from '../data/dataPhantom.json';
-import benchmarkData from '../data/benchmarkData.json';
-import singleWellData from '../data/singleWellData.json';
-import benchmarkList from '../data/benchmarkList';
+import benchmarkNames from '../data/benchmarkNames';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    data,
-    dataPhantom,
-    benchmarkData,
-    singleWellData,
-    benchmarkList,
-    user: {
-      name: 'Dunder Mifflin',
-      avatar: 'dunderMifflin.png',
-    },
+    benchmarkNames,
     currentWell: dataPhantom.includedWells[0],
     wells: dataPhantom.includedWells,
   },
@@ -33,26 +22,15 @@ export default new Vuex.Store({
     },
   },
   getters: {
-    benchmarks: state => state.dataPhantom.benchmarkDetailsByFeet.filter(
-      benchmark => benchmark.name === 'minDrilledHours',
+    benchmarks: (state, getters) => dataPhantom.benchmarkDetailsByFeet.filter(
+      benchmark => benchmark.name === 'minDrilledHours' && benchmark.startDepth <= getters.yMax,
     ),
-    sortedData: (state, getters) =>
-      getters.wellNames.map((wellNameNo) => {
-        const dataArray = state.data.filter(well => well.wellNameNo === wellNameNo)
-          .map(well => ({
-            cumulativeCost: well.cumulativeCost,
-            cumulativeDepth: well.cumulativeDepth,
-            cumulativeTime: well.cumulativeTime,
-          }));
-        return {
-          wellNameNo,
-          data: dataArray,
-        };
-      }),
-    wellNames: state =>
-      state.dataPhantom.includedWells.map(well => ({
+    wellNames: () =>
+      dataPhantom.includedWells.map(well => ({
         id: well.id,
         name: well.wellName,
       })),
+    yMax: state =>
+      Math.max(...state.currentWell.benchmarkInputByPortionInfo.map(well => well.startDepth)),
   },
 });
