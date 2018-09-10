a26575333ffb560368c7bee34bf8d94f98fb0c1b
Hanseul Shim <hanseulshim@gmail.com>
2018-09-10 11:59:16 -0400

    Move chart height and vertical margins to chartInfo module

--- src/store/modules/chartInfo.js
+++ src/store/modules/chartInfo.js
@@ -1,8 +1,14 @@
+import * as d3 from 'd3';
 import colors from '../../data/colors.json';
 
 export default {
   state: {
     colors,
+    layout: {
+      height: 600,
+      marginTop: 45,
+      marginBottom: 50,
+    },
   },
   getters: {
     maxCost: (state, getters) =>
@@ -17,5 +23,10 @@ export default {
       Math.max(...getters.slopeData.map(well => well.running_average_gradient_diff)),
     xMax: (state, getters, rootState) =>
       Math.max(...getters.wellData.map(well => well[rootState.options.currentCompare])),
+    yScale: (state, getters) =>
+      d3.scaleLinear()
+        .domain([0, getters.maxDepth])
+        .range([0, state.height])
+        .nice(),
   },
 };
