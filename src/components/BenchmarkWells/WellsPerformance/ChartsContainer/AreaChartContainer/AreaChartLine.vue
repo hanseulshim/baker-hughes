db67e0c4620922a02b08c0d576768ac402a6f70c
Hanseul Shim <hanseulshim@gmail.com>
2018-09-26 12:15:15 -0400

    Filter by selected well and selected well on area chart

--- src/components/BenchmarkWells/WellsPerformance/ChartsContainer/AreaChartContainer/AreaChartLine.vue
+++ src/components/BenchmarkWells/WellsPerformance/ChartsContainer/AreaChartContainer/AreaChartLine.vue
@@ -6,7 +6,6 @@
       <rect
         :width="this.layout.width"
         :height="this.layout.height"
-        fill="#F9F9F9"
       />
     </clipPath>
     <path
@@ -16,6 +15,7 @@
       :d="drawLine(well)"
       :fill="well.color"
       clip-path="url(#clip-area)"
+      :opacity="!selectedWell.wellName || well.wellName === selectedWell.wellName ? 1 : 0.2"
     />
   </g>
 </template>
@@ -43,28 +43,57 @@ export default {
     return {
       lineStyle: {
         strokeWidth: 1.5,
-        opacity: 0.6,
       },
     };
   },
   methods: {
     drawLine(well) {
       const filteredSlope = this.combinedSlopeData.filter(slope => slope.well === well.wellName);
+      const filterLine = [];
       const path = d3.area()
         .defined(d => d.running_average_gradient_diff > this.xDomain.min)
         .y1(d => this.scale.y(d.cumulativeDepth))
         .x0(d => this.scale.x(d.running_average_gradient_diff))
         .x1(this.xDomain.min)
         .y0(d => this.scale.y(d.cumulativeDepth));
-      return path(filteredSlope);
+      if (this.currentBitFilter.id === 'all') {
+        filterLine.push(...filteredSlope);
+      } else if (this.currentBitFilter.id === 'last') {
+        const filterDepth = well.drillBits[well.drillBits.length - 1].depthIn;
+        filterLine.push(...filteredSlope
+          .filter(depth => depth.cumulativeDepth >= filterDepth));
+      } else if (this.currentBitFilter.id - 1 >= well.drillBits.length) {
+        return path([]);
+      } else {
+        const drillBitIndex = this.currentBitFilter.id - 1;
+        const drillBitLastIndex = this.currentBitFilter.id;
+        const filterDepth = well.drillBits[drillBitIndex].depthIn;
+        const filterDepthLast = well.drillBits.length === this.currentBitFilter.id ?
+          Infinity : well.drillBits[drillBitLastIndex].depthIn;
+        filterLine.push(...filteredSlope
+          .filter(depth =>
+            depth.cumulativeDepth >= filterDepth && depth.cumulativeDepth <= filterDepthLast));
+      }
+      return path(filterLine);
     },
   },
   computed: {
     combinedSlopeData() {
       return this.$store.getters.combinedSlopeData;
     },
+    currentBitFilter() {
+      return this.$store.state.well.currentBitFilter;
+    },
+    selectedWell() {
+      return this.$store.state.well.selectedWell;
+    },
     wellList() {
-      return this.$store.getters.combinedWells.wellList;
+      const wellList = this.$store.getters.combinedWells.wellList
+        .filter(well => well.wellName !== this.selectedWell.wellName);
+      if (this.selectedWell.wellName) {
+        wellList.push(this.selectedWell);
+      }
+      return wellList;
     },
   },
   watch: {
